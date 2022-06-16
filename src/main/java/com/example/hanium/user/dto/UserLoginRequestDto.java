3fbd144e9757f587f8080006c234b88875ba20f2
pawoo0211 <pawoo0211@naver.com>
2022-06-16 22:10:23 +0900

    refactor: 회원가입, 로그인 유효성 검증 수정

--- src/main/java/com/example/hanium/user/dto/UserLoginRequestDto.java
+++ src/main/java/com/example/hanium/user/dto/UserLoginRequestDto.java
@@ -1,19 +1,16 @@
 package com.example.hanium.user.dto;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class UserLoginRequestDto {
 
-    @NotBlank(message = "이메일을 입력해주세요")
     @Email(message = "이메일 형식을 확인해주세요")
     private String email;
 
-    @NotBlank(message = "패스워드 입력해주세요")
-    @Range(min=4,max=12, message = "4~12 글자의 패스워드를 입력해주세요")
+    @Size(min=4,max=12, message = "4~12 글자의 패스워드를 입력해주세요")
     private String password;
 }
--- src/main/java/com/example/hanium/user/dto/UserRegisterRequestDto.java
+++ src/main/java/com/example/hanium/user/dto/UserRegisterRequestDto.java
@@ -1,7 +1,6 @@
 package com.example.hanium.user.dto;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -13,12 +12,10 @@ public class UserRegisterRequestDto {
     @NotBlank(message = "이름을 입력해주세요")
     private String name;
 
-    @NotBlank(message = "이메일을 입력해주세요")
     @Email(message = "이메일 형식을 확인해주세요")
     private String email;
 
-    @NotBlank(message = "패스워드 입력해주세요")
-    @Range(min=4,max=12, message = "4~12 글자의 패스워드를 입력해주세요")
+    @Size(min=4,max=12, message = "4~12 글자의 패스워드를 입력해주세요")
     private String password;
 
     @NotBlank(message = "전화번호를 입력해주세요")
